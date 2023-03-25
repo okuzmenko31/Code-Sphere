118b9b71685a2211b1732f864b7e889897d444e6
okuzmenko31 <olegkuzmenko2005@gmail.com>
2023-03-25 18:29:10 +0200

    Renaned project directory. Finished work on the welcome page header

--- codesphere/core/urls.py
+++ codesphere/core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tech-sphere/welcome/', include('home.urls'))
+    path('codesphere/welcome/', include('home.urls'))
 ]
