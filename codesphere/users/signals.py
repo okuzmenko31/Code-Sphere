2ec018bc3ab970d47dbe0e4d51b70101af15ea2b
okuzmenko31 <olegkuzmenko2005@gmail.com>
2023-04-10 15:57:12 +0300

    Fixed some conflicts with scripts

--- codesphere/users/signals.py
+++ codesphere/users/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User, UserProfile
-from django.conf import settings
 
 
 @receiver(post_save, sender=User)
